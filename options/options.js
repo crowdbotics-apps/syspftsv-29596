3a56c0c5eaaf3010e7001299734bf860ad841028
Crowdbotics <team@crowdbotics.com>
2021-08-07 08:06:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "syspftsv_29596Identifier",
+  url: "https://syspftsv_29596Identifier.botics.co",
+  api: "https://syspftsv_29596Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
